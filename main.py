75ca4e22d9e8df401acf230ed42aad7087f8a7b9
jwin7 <139569767+jwin7@users.noreply.github.com>
2023-08-07 01:44:06 -0300

    .

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from views.vista_inicio import VentanaInicio
 
 if __name__ == "__main__":
     root = Tk()
-    root.title("App de Eventos")
+    root.title("App Tour Festival Pais")
     root.geometry("400x300")
 
     ventana_inicio = VentanaInicio(root)
