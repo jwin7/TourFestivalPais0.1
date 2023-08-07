a14188679c2736f47eeca9af814e150edde9bc28
jwin7 <139569767+jwin7@users.noreply.github.com>
2023-08-07 01:38:44 -0300

    .

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from views.vista_inicio import VistaInicio
-from views.vista_eventos import VistaEventos
-from controllers.controlador_usuarios import ControladorUsuarios
 
-
-def main():
-    vista_inicio = VistaInicio()
-    controlador_usuarios = ControladorUsuarios()
-
-    # Llamar a la función mostrar_ventana_inicio() y almacenar el objeto VistaInicio en una variable
-    ventana_inicio = vista_inicio.mostrar_ventana_inicio(controlador_usuarios)
-
-    # Aquí puedes utilizar la variable ventana_inicio para acceder a métodos o atributos de la clase VistaInicio si es necesario
+from tkinter import Tk
+from views.vista_inicio import VentanaInicio
 
 if __name__ == "__main__":
-    main()
+    root = Tk()
+    root.title("App de Eventos")
+    root.geometry("400x300")
+
+    ventana_inicio = VentanaInicio(root)
+
+    root.mainloop()
