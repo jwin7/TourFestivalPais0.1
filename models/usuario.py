99d05007a1ad72d1803f017336540cad374b5ecb
jwin7 <139569767+jwin7@users.noreply.github.com>
2023-08-07 01:43:51 -0300

    .

--- models/usuario.py
+++ models/usuario.py
@@ -1,10 +1,11 @@
 import json
 
 class Usuario:
-    def __init__(self, id, nombre, apellido, historial_eventos):
+    def __init__(self, id, nombre, apellido, contrasena, historial_eventos):
         self.id = id
         self.nombre = nombre
         self.apellido = apellido
+        self.contrasena = contrasena
         self.historial_eventos = historial_eventos
 
     def to_json(self):
@@ -12,6 +13,7 @@ class Usuario:
             "id": self.id,
             "nombre": self.nombre,
             "apellido": self.apellido,
+            "contrasena": self.contrasena,
             "historial_eventos": self.historial_eventos
         }
 
@@ -22,6 +24,7 @@ class Usuario:
             data["id"],
             data["nombre"],
             data["apellido"],
+            data["contrasena"],
             data["historial_eventos"]
             )
 
