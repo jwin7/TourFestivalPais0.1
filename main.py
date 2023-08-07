43923bddc913722104c151e0c88d7e53f1fbda20
jwin7 <139569767+jwin7@users.noreply.github.com>
2023-08-06 23:44:27 -0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import customtkinter as tk
+import json
 import tkinter.ttk as ttk
 import tkinter.messagebox as messagebox
 import tkinter.filedialog as filedialog
@@ -10,23 +10,29 @@ from models.review import Review
 from models.usuarios import Usuario
 from PIL import Image, ImageTk
 import requests
-import json
 from views.vista_inicio import VistaInicio
 from views.vista_eventos import VistaEventos
 from views.vista_eventos import VistaEvento
 from controllers.controlador_usuarios import ControladorUsuarios
 from controllers.controlador_eventos import ControladorEventos
-
+from views.vista_inicio import VistaInicio
+from views.vista_eventos import VistaEventos
+from controllers.controlador_usuarios import ControladorUsuarios
 
 def main():
-    app = App()
-    app.mainloop() 
-
     vista_inicio = VistaInicio()
     controlador_usuarios = ControladorUsuarios()
 
-    # Aquí se inicia la aplicación, por ejemplo, mostrar la ventana de inicio de sesión
-    vista_inicio.mostrar_ventana_inicio(controlador_usuarios)
+    
+    usuario = vista_inicio.mostrar_ventana_inicio(controlador_usuarios)
+
+    eventos_asistidos = controlador_usuarios.obtener_eventos_asistidos(usuario)
+
+    if eventos_asistidos:
+        vista_eventos = VistaEventos(usuario)
+        vista_eventos.mostrar_ventana_eventos(eventos_asistidos)
+    else:
+        print("No se encontraron eventos asistidos para el usuario")
 
 if __name__ == "__main__":
     main()
